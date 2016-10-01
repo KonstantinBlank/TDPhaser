3db6200938dfab0c6f575f9eec5641f432eb918c
KonstantinBlank <konstantin.blank@web.de>
2016-10-01 12:49:48 +0200

    turret: basic methodes

--- js/turret_Prefab.js
+++ js/turret_Prefab.js
@@ -1,9 +1,50 @@
-turret_Prefab = function (pName, pDamage, pAttackspeed, pRange)
+function turret_Prefab (pName, pDamage, pAttackspeed, pRange)
 {
     var _Name = pName;
     var _Damage = pDamage;
     var _AttackSpeed = pAttackspeed;
     var _Range = pRange; // als Radius
+
+    this.findEnemy = function()
+    {
+        //findet enemy
+        //ruft attackEnemy auf und übergibt pEnemy
+    }
+
+    this.attackEnemy = function(pEnemy)
+    {
+        pEnemy.dealDmg(_Damage);
+    }
+
+    //upgrades
+        this.upgradeDamage = function()
+        {
+            _Damage++;
+            
+        }
+
+        this.uprageRange = function()
+        {
+            _Range++;
+        }
+
+        this.upgradeAttackSpeed = function()
+        {
+            _AttackSpeed++;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     this.teste = function()
     {
